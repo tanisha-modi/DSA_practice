bca67c2bdd5f1d83fb7a9b0d098cacf1257b374d
tanisha <tanishamodi12@gmail.com>
2023-04-11 23:44:15 +0530

    hashing

---
+++ Hashing/HashMap1.java
@@ -0,0 +1,53 @@
+package Hashing;
+import java.rmi.server.RemoteServer;
+import java.util.*;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class HashMap1 {
+    public static void main(String[] args) {
+        
+        // creating
+        HashMap<String, Integer> map = new HashMap<>();   // Unordered map //pairs
+
+        // Insertion
+        map.put("India", 120);
+        map.put("US", 30);
+        map.put("china", 150);
+
+        System.out.println(map);
+        
+        map.put("china", 180);  // value will update for the key "china"
+        System.out.println(map);
+
+
+        // search  -> contaisKey - returns true and false 
+        if (map.containsKey("UK")) {
+            System.out.println("key is present in the map");
+        }
+        else{
+            System.out.println("key is not present in map");
+        }
+        
+        // get function
+        System.out.println(map.get("India")); // -> returns key for the value 
+        System.out.println(map.get("UK")); // -> returns null as the key does not exist 
+
+        // iteration in HashMap
+        // for(int val : arr)
+        for( Map.Entry<String, Integer> val : map.entrySet()){
+             System.out.println(val.getKey());
+             System.out.println(val.getValue());
+        }
+
+        // another way for iteration 
+        Set<String> keys = map.keySet();
+        for(String s : keys){
+            System.out.println(s + " : " +  map.get(s));
+        }
+
+        // Remove 
+        map.remove("china");
+        System.out.println(map);
+    }
+}
---
+++ Hashing/HashSet1.java
@@ -0,0 +1,65 @@
+package Hashing;
+import java.util.*;
+// import java.util.HashSet;
+// import java.util.Iterator;
+
+public class HashSet1 {
+
+    public static void main(String[] args) {
+        
+        // creating --> same as ArrayList or List
+        HashSet<Integer> set  = new HashSet<>();   // set does not have order 
+
+        // Insert 
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+
+        // size
+        System.out.println("size : " + set.size());
+
+        // print all elements of set
+        System.out.println(set);
+
+
+
+
+        // Search - contains
+        if (set.contains(1)) {
+            System.out.println("contains one");
+        }
+        if( set.contains(6)){
+            System.out.println("contains six");
+        }
+        else{
+            System.out.println("does not contain six");
+        }
+
+
+
+
+
+        // Delete 
+        set.remove(1);
+        if( set.contains(1)){
+            System.out.println("contains one");
+        }
+        else{
+            System.out.println("does not contain one");
+        }
+
+
+
+        // Iterator 
+        Iterator it = set.iterator();
+        // 2 functions --> hasNext - returns true or false ; next - returns next value 
+        while(it.hasNext()){
+            System.out.println(it.next());  // order 
+        }
+    }
+
+
+
+}
