aecef1bb1825b028722cf9fca7307d80ae507fc4
tanisha <tanishamodi12@gmail.com>
2023-09-17 23:53:25 +0530

    linked list

---
+++ LinkedList_java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(34);
+
+        list.display();
+    }
+}
